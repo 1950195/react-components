5fd88ce70a4c249a18dc33b9d32792906759ed9b
Lionel Dong <1950195@qq.com>
2019-06-27 21:20:31 +0800

    feat: 🎸 petty prop-type for table component

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,44 +1,36 @@
 import React from 'react';
 import { Table as BSTable } from 'reactstrap';
 
-interface IRowData {
-  [key: string]: any;
-}
-
-interface IColumn {
-  title: string;
-  key: string;
-}
-
-interface IRowProps<RowData extends IRowData, Column extends IColumn> {
-  rowData: RowData;
-  columns: Column[];
-}
-
-export interface ITableProps<RowData extends IRowData, Column extends IColumn> {
+export interface ITableProps {
+  /**
+   * display with striped
+   *
+   * @default false
+   */
   striped?: boolean;
+  /**
+   * display with bordered
+   *
+   * @default false
+   */
   bordered?: boolean;
-  dataSource: RowData[];
-  columns: Column[];
+  dataSource: Array<{ [key: string]: any; }>;
+  columns: Array<{ title: string; key: string; }>;
 }
 
-const Row = ({ rowData, columns }: IRowProps<IRowData, IColumn>) => (
-  <tr>
-    {columns.map(({ key }: IColumn) => <td key={`td-${key}`}>{rowData[key]}</td>)}
-  </tr>
-);
-
-export const Table = ({ striped = false, bordered = false, dataSource, columns }: ITableProps<IRowData, IColumn>) => {
+export const Table = ({ striped = false, bordered = false, dataSource, columns }: ITableProps) => {
   return (
     <BSTable bordered={bordered} striped={striped}>
       <thead>
         <tr>
-          {columns.map(({ title }: IColumn) => <th key={`thead-${title}`}>{title}</th>)}
+          {columns.map(({ title }) => <th key={`thead-${title}`}>{title}</th>)}
         </tr>
       </thead>
       <tbody>
-        {dataSource.map((rowData: IRowData, index: number) => (
-          <Row key={`tr-${index}`} {...{rowData, columns}} />
+        {dataSource.map((rowData, index: number) => (
+          <tr key={`tr-${index}`}>
+            {columns.map(({ key }) => <td key={`td-${key}`}>{rowData[key]}</td>)}
+          </tr>
         ))}
       </tbody>
     </BSTable>
